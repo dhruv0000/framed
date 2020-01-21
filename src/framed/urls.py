c820eb5e4241d52e426fdd63c9eea65369e8f175
killbotxd <sonawane.1@iitj.ac.in>
2020-01-21 14:42:59 +0530

    add gallery template

--- src/framed/urls.py
+++ src/framed/urls.py
@@ -19,6 +19,6 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = "landing.html"), name = "landing"),
+    path('', TemplateView.as_view(template_name="gallery.html"), name="landing"),
     path('admin/', admin.site.urls),
 ]
