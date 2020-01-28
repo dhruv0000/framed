6a7b9b470e75f3a3e8a14183aa7b08a49927e35d
dhruv0000 <patel.4@iitj.ac.in>
2020-01-29 02:31:25 +0530

    Fixed Landing

--- src/framed/urls.py
+++ src/framed/urls.py
@@ -19,6 +19,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="gallery.html"), name="landing"),
-    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="landing.html"), name="landing"),
+    path('gallery', TemplateView.as_view(template_name="gallery.html"), name="gallery"),
+    path('admin', admin.site.urls),
 ]
